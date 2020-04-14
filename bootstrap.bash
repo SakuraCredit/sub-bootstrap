11c6dfef2d24f119847cd0f2661a885426cd5cb2
boweiliu <15707615+boweiliu@users.noreply.github.com>
2020-04-13 23:02:38 -0700

    Create bootstrap.bash

---
+++ bootstrap.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+main() {
+    [ -d "$HOME"/.sub ] || \
+    git clone -q https://github.com/SakuraCredit/sub.git "$HOME"/.sub 2>/dev/null || \
+    git clone -q git@github.com:SakuraCredit/sub.git "$HOME"/.sub 2>/dev/null || \
+    git clone https://github.com/SakuraCredit/sub.git "$HOME"/.sub || \
+    git clone git@github.com:SakuraCredit/sub.git "$HOME"/.sub
+
+    if [ -f ~/.zshenv ] ; then
+        echo 'eval "$("$HOME"/.sub/bin/hatch init -)"' >> ~/.zshenv
+    fi
+    if [ -f ~/.bashrc ] ; then
+        echo 'eval "$("$HOME"/.sub/bin/hatch init -)"' >> ~/.bashrc
+    fi
+    /bin/bash -c '"$HOME"/.sub/bin/hatch install-packages'
+}
+
+main
