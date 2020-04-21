fc97ad622b5e5e4d063bccf48254430a245f659f
Nikhil Gupta <niks.gupta09@gmail.com>
2020-04-21 09:27:27 -0400

    zshenv -> zshrc

--- bootstrap.bash
+++ bootstrap.bash
@@ -7,19 +7,19 @@ main() {
     git clone https://github.com/SakuraCredit/sub.git "$HOME"/.sub || \
     git clone git@github.com:SakuraCredit/sub.git "$HOME"/.sub
 
-    if [ -f ~/.zshenv ] ; then
-        echo 'eval "$("$HOME"/.sub/bin/hatch init -)"' >> ~/.zshenv
+    if [ -f ~/.zshrc ] ; then
+        echo 'eval "$("$HOME"/.sub/bin/hatch init -)"' >> ~/.zshrc
     fi
     if [ -f ~/.bashrc ] ; then
         echo 'eval "$("$HOME"/.sub/bin/hatch init -)"' >> ~/.bashrc
     fi
-    /bin/bash -c '"$HOME"/.sub/bin/hatch install-packages'
+    /bin/bash -c '"$HOME"/.sub/bin/hatch bootstrap-workstation'
 }
 
 echo
 echo '=> Close and reopen your terminal, or run the following:'
 echo
 echo '(Bash) $ source ~/.bashrc'
-echo '(Zsh) % source ~/.zshenv'
+echo '(Zsh) % source ~/.zshrc'
 
 main
